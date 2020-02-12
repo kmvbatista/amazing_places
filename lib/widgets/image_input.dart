939c369773ed4c337c5a278c1f7200948bb9d2c6
Kennedy <kennedymessias.vb@gmail.com>
2020-02-12 19:06:00 -0300

    image input

---
+++ lib/widgets/image_input.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'dart:io';
+
+class ImageInput extends StatefulWidget {
+  @override
+  _ImageInputState createState() => _ImageInputState();
+}
+
+class _ImageInputState extends State<ImageInput> {
+  File _storedImage;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Container(
+          width: 100,
+          height: 100,
+          decoration: BoxDecoration(
+            border: Border.all(width: 1, color: Colors.grey),
+          ),
+          child: _storedImage == null
+              ? Image.file(
+                  _storedImage,
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                )
+              : Text(
+                  'No image taken',
+                  textAlign: TextAlign.center,
+                ),
+        ),
+        SizedBox(
+          width: 10,
+        ),
+        Expanded(
+          child: FlatButton.icon(
+            onPressed: null,
+            icon: Icon(Icons.camera),
+            label: Text('Take picture'),
+            textColor: Theme.of(context).primaryColor,
+          ),
+        )
+      ],
+    );
+  }
+}
