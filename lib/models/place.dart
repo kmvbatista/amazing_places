e5e9cc62debc26b88b5e1318f0be4d188c30b052
Kennedy <kennedymessias.vb@gmail.com>
2020-02-12 17:48:18 -0300

    place model

---
+++ lib/models/place.dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+
+class Place {
+  final String id;
+  final String title;
+  final File image;
+  final PlaceLocation location;
+
+  Place(
+      {@required this.id,
+      @required this.title,
+      @required this.image,
+      @required this.location});
+}
+
+class PlaceLocation {
+  final double latitude;
+  final double longitude;
+  final String address;
+
+  PlaceLocation({this.latitude, this.longitude, this.address});
+}
