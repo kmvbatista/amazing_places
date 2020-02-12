36d4c846f5117c629ebc80cdfea310bf79988a6f
Kennedy <kennedymessias.vb@gmail.com>
2020-02-12 19:06:10 -0300

    add place screen

--- lib/pages/add_place_screen.dart
+++ lib/pages/add_place_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:great_places/widgets/image_input.dart';
 
 class AddPlaceScreen extends StatefulWidget {
   static final String routeName = 'add-place';
@@ -7,6 +8,7 @@ class AddPlaceScreen extends StatefulWidget {
 }
 
 class _AddPlaceScreenState extends State<AddPlaceScreen> {
+  final _titleController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,9 +16,32 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
         title: Text('Add a New Place'),
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          Text('User Inputs...'),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  children: <Widget>[
+                    TextField(
+                      controller: _titleController,
+                      decoration: InputDecoration(labelText: 'Title'),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    ImageInput()
+                  ],
+                ),
+              ),
+            ),
+          ),
           RaisedButton.icon(
+              materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+              elevation: 0,
+              color: Theme.of(context).accentColor,
               icon: Icon(Icons.add),
               label: Text('Add Place'),
               onPressed: () {}),
