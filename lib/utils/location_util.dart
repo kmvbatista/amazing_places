1c566e2ee087a587523dc7df3ff6d644a068a68f
Kennedy <kennedymessias.vb@gmail.com>
2020-02-14 14:01:00 -0300

    google location configuration

---
+++ lib/utils/location_util.dart
@@ -0,0 +1,8 @@
+const GOOGLE_API_KEY = 'AIzaSyAkrVKuE7Lymy2SYzMnsOTgigw-kmJ0al0';
+
+class LocationUtil {
+  static String generateLocationPreviewImage(
+      {double latitude, double longitude}) {
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=&$latitude,$longitude=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:C%7C$latitude,$longitude&key=YOUR_API_KEY';
+  }
+}
