38bf85c2b32f43457ecc02520b9c4edc2a7f5a2b
Kennedy <kennedymessias.vb@gmail.com>
2020-02-14 13:12:12 -0300

    location input

---
+++ lib/widgets/location_input.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class LocationInput extends StatefulWidget {
+  @override
+  _LocationInputState createState() => _LocationInputState();
+}
+
+class _LocationInputState extends State<LocationInput> {
+  String _previewImageUrl;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Container(
+          height: 170,
+          width: double.infinity,
+          child: _previewImageUrl == null
+              ? const Text(
+                  'No location choosen',
+                  textAlign: TextAlign.center,
+                )
+              : Image.network(
+                  _previewImageUrl,
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                ),
+        ),
+        Row(
+          children: <Widget>[
+            FlatButton.icon(
+              onPressed: null,
+              icon: Icon(Icons.location_on),
+              label: Text('Current location'),
+              textColor: Theme.of(context).primaryColor,
+            ),
+            FlatButton.icon(
+              onPressed: null,
+              icon: Icon(Icons.location_on),
+              label: Text('Select on Map'),
+              textColor: Theme.of(context).primaryColor,
+            ),
+          ],
+        )
+      ],
+    );
+  }
+}
